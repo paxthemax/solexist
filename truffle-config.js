717c259a1b27d70c3dd7105e327084a7ecb6435d
Pavle Batuta <pavledb@gmail.com>
2018-07-25 14:57:01 +0200

    Added eslint support
    Configured such as in: https://github.com/OpenZeppelin/openzeppelin-solidity/blob/master/truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,14 @@
 require('babel-register')({
-  ignore: /node_modules\/(?!zeppelin-solidity\/test\/helpers)/
+  ignore: /node_modules\/(?!zeppelin-solidity\/test\/helpers)/,
 });
 require('babel-polyfill');
 
 module.exports = {
   networks: {
     development: {
-      host: "127.0.0.1",
-      port: "8545",
-      network_id: "*"
-    }
-  }
+      host: '127.0.0.1',
+      port: '8545',
+      network_id: '*', // eslint-disable-line camelcase
+    },
+  },
 };
