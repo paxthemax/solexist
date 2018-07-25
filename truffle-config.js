4167ccb65992794204ba1d02f4a495bdaf7425e4
Pavle Batuta <pavledb@gmail.com>
2018-07-25 12:52:03 +0200

    Add ganache test script
    Configured to run with yarn test

--- truffle-config.js
+++ truffle-config.js
@@ -4,5 +4,11 @@ require('babel-register')({
 require('babel-polyfill');
 
 module.exports = {
-  
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: "8545",
+      network_id: "*"
+    }
+  }
 };
