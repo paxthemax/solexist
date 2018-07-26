7185060a3ffc90f36460bdace88b510308ef1a9d
Pavle Batuta <pavledb@gmail.com>
2018-07-26 16:58:07 +0200

    Add suport to deploy via Infura
    Currently, ropsten is supported.
    
    Make sure to set the seed and API key in the .env file!

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
 require('babel-register')({
   ignore: /node_modules\/(?!zeppelin-solidity\/test\/helpers)/,
 });
@@ -10,5 +12,16 @@ module.exports = {
       port: '8545',
       network_id: '*', // eslint-disable-line camelcase
     },
-  },
+    // Testnets
+    // Properties
+    // network_id: identifier for network based on ethereum blockchain. Find out more at https://github.com/ethereumbook/ethereumbook/issues/110
+    // gas: gas limit
+    // gasPrice: gas price in gwei
+    ropsten_infura: {
+      provider: new HDWalletProvider(process.env.MNENOMIC, "https://ropsten.infura.io/" + process.env.INFURA_API_KEY),
+      network_id: 3,
+      gas: 3000000,
+      gasPrice: 21
+    }
+  }
 };
